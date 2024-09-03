2ff6b62ed6a1c620de834745d268ccb6c511da53
MaryAnn04 <maryannbarnabas4@gmail.com>
2024-09-03 10:45:59 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -8,5 +8,17 @@ TEST(SoundexTest, HandlesEmptyString) {
 
 TEST(SoundexTest, HandlesSingleCharacter) {
     EXPECT_EQ(generateSoundex("A"), "A000");
+    EXPECT_EQ(generateSoundex("R"), "R000");
 }
-
+TEST(SoundexTest, HandlesVowels) {
+    EXPECT_EQ(generateSoundex("AEIOU"), "A000");
+}
+TEST(SoundexTest, Handleslongstring) {
+    EXPECT_EQ(generateSoundex("ALEXANDER"), "A425");
+}
+TEST(SoundexTest, HandlesIgnoredConsonents) {
+    EXPECT_EQ(generateSoundex("HWY"), "H000");
+}
+TEST(SoundexTest, HandlesRepeatedCharacter) {
+    EXPECT_EQ(generateSoundex("BUTTERFLY"), "B336");
+}
